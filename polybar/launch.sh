22e5d60bedeeca87ad7a0fffba2d55b42ea86f2e
OPhamster <OPhamster@users.noreply.github.com>
2021-08-16 12:54:00 +0530

    UPDATE: multi-monitor support for polybar + workspace locality fix
    * render the bar for every connected monitor
    * render the workspace labels only for the workspaces local to the
      display its on.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,4 +4,7 @@
 killall -q polybar
 
 # launch bar
-polybar example 2>&1 > /var/log/polybar/bar.log & disown
+monitors=$(xrandr --query | grep \ connected | cut -d " " -f1)
+for monitor in $monitors; do
+    MONITOR=$monitor polybar example 2>&1 > /var/log/polybar/${monitor}_bar.log & disown
+done
