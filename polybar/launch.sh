9622c1620c10072ccc55345baef362394f8d9e99
OPhamster <OPhamster@users.noreply.github.com>
2021-08-19 12:21:56 +0530

    UPDATE: picom + systray display + drop unused pb modules
    * update picom in an effort to make in useable in multiple-displays.
      no effect. Added a few other sensible defaults like dimmed
      inactive windows.
      TODO: understand the source of the problem is it really picom ?
      It isn't the application itself since the slow startup of
      applications like `firefox` only occur on multiple-displays.
    * show the systray only on the primary display - no use showing
      it on every display.
    * drop polybar module definitions that were unused since the
      previous commit.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,6 +5,12 @@ killall -q polybar
 
 # launch bar
 monitors=$(xrandr --query | grep \ connected | cut -d " " -f1)
+# the systray should only show up on the primary display
+systray_monitor=$(xrandr --query | grep \ primary | cut -d " " -f1)
 for monitor in $monitors; do
-    MONITOR=$monitor polybar example 2>&1 > /var/log/polybar/${monitor}_bar.log & disown
+    if [ ${monitor} == ${systray_monitor} ]; then
+        MONITOR=$monitor SYSTRAY_POSITION=right polybar example 2>&1 > /var/log/polybar/${monitor}_bar.log & disown
+    else
+        MONITOR=$monitor polybar example 2>&1 > /var/log/polybar/${monitor}_bar.log & disown
+    fi
 done
