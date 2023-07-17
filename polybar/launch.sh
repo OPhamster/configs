fb87e4444d86d539e832a23326d9917d0a4ba0c0
ophamster <maitra.neel@gmail.com>
2023-07-17 22:30:06 +0530

    ft(bar): Switch to polybar
    * Also adjust gaps to stick to the larger aesthetic.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
 
-# launch bar
-monitors=$(xrandr --query | grep \ connected | cut -d " " -f1)
-# the systray should only show up on the primary display
-systray_monitor=$(xrandr --query | grep \ primary | cut -d " " -f1)
-for monitor in $monitors; do
-    if [ ${monitor} == ${systray_monitor} ]; then
-        MONITOR=$monitor SYSTRAY_POSITION=right polybar top 2>&1 > /var/log/polybar/${monitor}_bar.log & disown
-    else
-        MONITOR=$monitor polybar top 2>&1 > /var/log/polybar/${monitor}_bar.log & disown
-    fi
-done
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log
+polybar bottom 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
