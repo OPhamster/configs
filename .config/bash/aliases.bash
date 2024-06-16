ecf6afe98b38235e1dc6de09e250c8bc815cb488
OPhamster <maitra.neel@gmail.com>
2024-06-16 11:59:57 +0530

    ft(bash): Use `pinfo` as `info` viewer

--- .config/bash/aliases.bash
+++ .config/bash/aliases.bash
@@ -12,3 +12,9 @@ if [[ $? -eq 0 ]]; then
     export MANROFFOPT="-c" MANPAGER="sh -c 'col -bx | ${BAT} -l man -p --theme Coldark-Dark'"
     alias cat='${BAT} --paging=never'
 fi
+
+# pinfo
+type -p pinfo > /dev/null
+if [[ $? -eq 0 ]]; then
+    alias info=pinfo
+fi
