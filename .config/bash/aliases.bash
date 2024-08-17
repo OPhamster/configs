de7f5522709bedecb9313af78e45c5fff5d37e6a
OPhamster <maitra.neel@gmail.com>
2024-08-17 19:07:54 +0530

    ft(bash): Add paging alternative `catp`

--- .config/bash/aliases.bash
+++ .config/bash/aliases.bash
@@ -11,6 +11,7 @@ type -p $BAT > /dev/null
 if [[ $? -eq 0 ]]; then
     export MANROFFOPT="-c" MANPAGER="sh -c 'col -bx | ${BAT} -l man -p --theme Coldark-Dark'"
     alias cat='${BAT} --paging=never'
+    alias catp='${BAT} --paging=always'
 fi
 
 # pinfo
