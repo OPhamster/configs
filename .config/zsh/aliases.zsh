5d66711f603a28a935691e50840f6ea5ee9d38e2
OPhamster <maitra.neel@gmail.com>
2024-09-07 22:13:38 +0530

    ft(kitty): Default to `zsh` from `bash`

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,30 @@
+# bat
+
+BAT=bat
+# debian systems have this bin as `batcat`
+type -p apt > /dev/null
+if [[ $? -eq 0 ]]; then
+    alias BAT=batcat
+fi
+
+type -p $BAT > /dev/null
+if [[ $? -eq 0 ]]; then
+    export MANROFFOPT="-c" MANPAGER="sh -c 'col -bx | ${BAT} -l man -p --theme Coldark-Dark'"
+    alias cat='${BAT} --paging=never'
+    alias catp='${BAT} --paging=always'
+fi
+
+# pinfo
+type -p pinfo > /dev/null
+if [[ $? -eq 0 ]]; then
+    alias info=pinfo
+fi
+
+# kitty integration
+if [[ $TERM == "xterm-kitty" ]]; then
+    alias ssh='kitten ssh'
+fi
+
+alias convert=magick
+
+alias vim="emacsclient -c -a=''"
