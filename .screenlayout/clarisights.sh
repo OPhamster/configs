b241caa6725cb4e82f0918507fd7b70711c17873
OPhamster <neel.maitra@clarisights.com>
2024-10-05 01:25:50 +0530

    fix(randr): Update office devices

--- .screenlayout/clarisights.sh
+++ .screenlayout/clarisights.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-
-if [[ $(xrandr -q | grep ' connected' | wc -l) -eq 2 ]]; then
-	xrandr --output eDP-1 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal
-fi
+xrandr --output eDP --mode 1920x1200 --pos 2560x120 --rotate normal --output HDMI-A-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
