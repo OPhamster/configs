93bd87e92d3ecb5873607e1a2462cf47a024d0ba
OPhamster <neel.maitra@clarisights.com>
2025-06-21 14:39:58 +0530

    fix(scripts): Switch to 4k with appropriate scaling on ext display

--- .screenlayout/clarisights_external.sh
+++ .screenlayout/clarisights_external.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output eDP --off --output HDMI-A-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
+xrandr --output eDP --off --output HDMI-A-0 --scale 0.80 --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
 nitrogen --restore
