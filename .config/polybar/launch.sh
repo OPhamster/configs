8a5f74e054e3e762b042c0ff04a7dc5fe2b381c7
OPhamster <neel.maitra@clarisights.com>
2026-01-24 12:26:49 +0530

    rft(i3): Theme updates + qol updates for mode changes etc

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,9 +2,11 @@
 
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+# polybar-msg cmd quit
+# Otherwise you can use the nuclear option (the one above doesn't seem to work,
+# keeps orphaned polybar instances running for e.g we see multiple bars in the top part):
+# TODO: this doesn't work as expected, we still have orphaned polybar instances.
+killall -q polybar
 
 # Launch bar1 and bar2
 for monitor in $(polybar -M | cut -d ':' -f 1);
