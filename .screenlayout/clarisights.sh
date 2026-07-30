52b12298b43b9c858d7b55422af0474e59688944
OPhamster <neel.maitra@clarisights.com>
2026-07-30 09:57:28 +0530

    rft(xrandr): Fix work layouts
    * still doesn't work as expected, will need to debug

--- .screenlayout/clarisights.sh
+++ .screenlayout/clarisights.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr --output eDP --mode 1920x1200 --pos 320x1440 --rotate normal --output HDMI-A-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
+xrandr --output eDP --mode 1920x1200 --pos 3840x960 --rotate normal --output HDMI-A-0 --primary --off --output DisplayPort-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
 
 # wallpaper
 nitrogen --restore
